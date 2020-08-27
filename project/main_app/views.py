bdc658cd7433735eae42210398899f6c37406e2c
hanum-lee <hanum.lee1@gmail.com>
2020-08-27 16:16:50 -0600

    Adding frontend to main_app

--- project/main_app/views.py
+++ project/main_app/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'main_app/index copy.html')
+
+def signin(request):
+    return render(request, 'main_app/src/sign-in/signin.html')
