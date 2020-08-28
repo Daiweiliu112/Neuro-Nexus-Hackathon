dd93b14529b07b0567b6261da49b8f61c7b02c1e
Paul Saunders <psaunder@ualberta.ca>
2020-08-27 20:08:16 -0600

    Refactored JS, prepped for implementing boxes

--- project/staticfiles/js/game.js
+++ project/staticfiles/js/game.js
@@ -76,9 +76,24 @@ function initDraw(canvas) {
         return inside
     }
     
-    canvas.onclick = function (e) {
+    canvas.addEventListener('mousedown', e => {
         if (drawSwitch) {
-            if (element !== null && !newRectIntersect(element)) {
+            mouse.startX = mouse.x;
+            mouse.startY = mouse.y;
+            element = document.createElement('div');
+            element.className = 'rectangle'
+            element.style.left = mouse.x + 'px';
+            element.style.top = mouse.y + 'px';
+            canvas.appendChild(element)
+            
+            
+            canvas.style.cursor = "crosshair";
+        }
+    })
+    
+    canvas.addEventListener('mouseup', e => {
+        if (drawSwitch) {
+            if (element && !newRectIntersect(element)) {
                 var title = document.createElement('div')
                 var inp = document.createElement('input')
                 
@@ -86,32 +101,19 @@ function initDraw(canvas) {
                 title.classList.add('center-horizontal')
                 title.classList.add('center-vertical')
                 element.appendChild(title)
-
+        
                 inp.classList.add('rectangle-title')
                 inp.placeholder = 'No value entered'
-                inp.value = null
                 inp.type = 'text'
-                
-                drawSwitch = false;
-                element = null;
-                canvas.style.cursor = "default";
-                
+                                
                 title.appendChild(inp)
-                inp.click()
             } else if (element !== null) {
                 console.log('Please construct the rectangles such that they do not intersect')
-            } else {
-                mouse.startX = mouse.x;
-                mouse.startY = mouse.y;
-                element = document.createElement('div');
-                element.className = 'rectangle'
-                element.style.left = mouse.x + 'px';
-                element.style.top = mouse.y + 'px';
-                canvas.appendChild(element)
-                
-                canvas.style.cursor = "crosshair";
+                element.remove()
             }
+            element = null
+            canvas.style.cursor = "default";
         }
-        
-    }
+        drawSwitch = false;
+    })
 }
