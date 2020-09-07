e330dd12d01f2986ac019f4c462815a65d6bf73b
hanum-lee <hanum.lee1@gmail.com>
2020-09-07 15:04:20 -0600

    Setting up front end with django template

--- project/main_app/urls.py
+++ project/main_app/urls.py
@@ -6,8 +6,13 @@ app_name = 'main_app'
 urlpatterns = [
     path('',views.index,name='index'),
     path('sign_in/',views.signin,name='sign_in'),
+    path('sign_in_test/',views.signin_test,name='sign_in_test'),
     path('dash_board/',views.dashboard,name='dashboard'),
+    path('dashboard/',views.dashboard_test,name='dashboard_test'),
     path('clinician/',views.clinician_image,name='clinician'),
-    path('child/',views.child_image,name='child')
+    path('clinician_test/',views.child_image_test,name='clinician_test'),
+    path('child/',views.child_image,name='child'),
+    path('child_test/',views.child_image_test,name='child_test'),
+    path('signup/',views.signup,name='signup')
 
 ]
--- project/main_app/views.py
+++ project/main_app/views.py
@@ -8,11 +8,23 @@ def index(request):
 def signin(request):
     return render(request, 'main_app/src/sign-in/signin.html')
 
+def signin_test(request):
+    return render(request, 'main_app/src/sign-in/sign_in.html')
+
 def dashboard(request):
     return render(request,'main_app/src/dashboard/dashboard.html')
+
+def dashboard_test(request):
+    return render(request,'main_app/src/dashboard/dashboard_.html')
 
 def child_image(request):
     return render(request, 'main_app/src/child-image/child-image.html')
 
+def child_image_test(request):
+    return render(request, 'main_app/src/child-image/child_image.html')
+
 def clinician_image(request):
     return render(request, 'main_app/src/clinician-image/clinician-image.html')
+
+def signup(request):
+    return render(request,'main_app/src/create_account/create_account.html')
