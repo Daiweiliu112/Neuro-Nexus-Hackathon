511fb90917ecabdeb75dd6704e7fac0f333235df
Existentialist-Robot <eredman@ualberta.ca>
2020-09-22 15:53:01 -0600

    back-end check of cli num

--- project/main_app/views.py
+++ project/main_app/views.py
@@ -2,8 +2,33 @@ from django.shortcuts import render, redirect
 from .forms import UploadFileForm
 from . import utils
 from django.http import JsonResponse
+from accounts.models import (
+    Client,
+    ClientClinicianLink,
+    )
+from django.contrib.auth.models import User
+
 # Create your views here.
 
+def check_cli_num(request):
+    # request should be ajax and method should be GET.
+    if request.is_ajax and request.method == "GET":
+        # get the client number from the client side.
+        cli_num = request.GET.get("cli_num", None)
+        # check for the existing client number in the database.
+        user = request.user
+        if Client.objects.filter(cli_num = cli_num).exists():
+            # if cli_num found return not valid .
+            return JsonResponse({"valid":False}, status = 200)
+        else:
+            user = request.user
+            # if cli_num not found, then clinician can create a new client.
+            client = Client(clinician=user,id_num=cli_num)
+            client.save()
+            return JsonResponse({"valid":True}, status = 200)
+
+    return JsonResponse({}, status = 400)
+    
 def index(request):
     return render(request,'main_app/index copy.html')
 
