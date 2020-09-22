30fe0b8a0a3c9c5d79cd175cbc44b40c64e6b27e
hanum-lee <hanum.lee1@gmail.com>
2020-09-21 20:14:50 -0600

    unnecessary url removed

--- project/main_app/urls.py
+++ project/main_app/urls.py
@@ -9,19 +9,11 @@ urlpatterns = [
     #path('sign_in_test/',views.signin_test,name='sign_in_test'),
     path('dashboard_uploaded/',views.dashboard,name='dashboard_test'),
     path('dashboard/',views.dashboard_test,name='dashboard'),
-    path('clinician/',views.clinician_image,name='clinician'),
-    path('clinician_test/',views.clinician_game,name='clinician_test'),
-    path('child/',views.child_image,name='child'),
     path('child_test/',views.child_image_test,name='child_test'),
     #path('signup/',views.signup,name='signup'),
     path('csv/',views.download_csv,name='download'),
     path('upload/',views.upload_file,name='upload'),
     path(r'ajax/create_meeting/',views.make_meeting,name='create_meeting'),
-    #path('cli/<str:room_name>/',views.clinician_test,name='clinician_test'),
-    #path('client/<str:room_name>/',views.client_test,name='client_test')
-
-
-    path('client/',views.client_game,name='client'),
     #path('clinician/',views.clinician_game,name='clinician'),
     #path('cli/<str:room_name>/',views.clinician_test,name='clinician_test'),
     #path('client/<str:room_name>/',views.client_test,name='client_test'),
