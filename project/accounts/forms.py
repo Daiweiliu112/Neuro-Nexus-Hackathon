efbd05c4c8800fb022a70b98c83ea7c2ba8ad0a8
hanum-lee <chococloudy@gmail.com>
2020-10-20 17:01:19 -0600

    Documenting for accounts finished

--- project/accounts/forms.py
+++ project/accounts/forms.py
@@ -2,12 +2,15 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
+# This form is for the initial registeration of the user
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
 
+    # It uses email as username for registering the user
     class Meta:
         model = User
         fields = ["first_name","last_name","email","password1","password2"]
 
+# This form is for loggin in the user. It takes email as the username to authenticating
 class UserAuthForm(AuthenticationForm):
     username = forms.EmailField(label="Email")
